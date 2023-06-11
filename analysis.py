3ac31057f6187d306aa2e4acdae2eb8689a1f3dd
JakubChorzepa <jakubchorzepa@gmail.com>
2023-06-11 19:38:39 +0200

    analyse price mean by days to flight, month and analyse frequent operators

--- analysis.py
+++ analysis.py
@@ -1,9 +1,16 @@
+# %%
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# %%
+
 
 # read data from csv to data frame
 FILE_PATH = "data_05_06_2023.csv"
 df = pd.read_csv(FILE_PATH)
 
+# %%
 # rename data columns
 df = df.rename(
     columns={
@@ -17,4 +24,36 @@ df = df.rename(
     }
 )
 
-print(df.head())
+# %%
+# price mean by days to flight
+
+priceMeanByDate = df.groupby("DaysToFlight").mean("Price")
+print(priceMeanByDate["Price"])
+
+# chart
+plt.plot(priceMeanByDate["Price"])
+plt.title("Wykres średnich cen zależny od dni do wylotu")
+plt.xlabel("Dni do wylotu")
+plt.ylabel("Cena")
+plt.show()
+
+# %%
+# price mean by month
+df["DepartureDate"] = pd.to_datetime(df["DepartureDate"])
+priceMeanByMonth = df.groupby(pd.Grouper(key = "DepartureDate", freq = "M")).mean("Price")
+priceMeanByMonth.index = priceMeanByMonth.index.strftime("%B")
+print(priceMeanByMonth)
+
+# chart
+plt.bar(priceMeanByMonth.index, priceMeanByMonth["Price"])
+plt.title("Wykres średnich cen zależny od miesiąca")
+plt.xlabel("Miesiąc")
+plt.ylabel("Średnia cena")
+plt.show()
+
+# %%
+# most frequent operator from 100 cheapest flights
+
+cheapestFlights = df.sort_values("Price").head(100)
+cheapestFlights = cheapestFlights.groupby("DepartureOperator").count()
+print(cheapestFlights)
